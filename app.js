90572361c4098db4f30a3b0ef24c9296ee8c8afe
LUJAINA ALI <lujainaali10@gmail.com>
2025-06-23 04:12:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+   // -------------------VARIABLES FOR STRINGS----------------//
+
+// 1. Declare a variable called username.
+
+let username;
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
+let myName = 'Lujaina Ali';
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+let message = "Hello World";
+alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+
+let stdName = "Lujaina Ali";
+let stdage = 17;
+let stdcourse = "web & app development";
+
+alert(stdName);
+alert(stdage);
+alert(stdcourse);
+
+
+// 5. Write a script to display the following alert using one JS
+// variable:
+
+let pizza = "Pizza\nPizz\nPiz\nPi\nP";
+alert(pizza);
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+let email = "lujainaali10@gmail.com";
+alert("My email is: " + email);
+
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+let book = "A smarter way to learn JavaScript";
+alert(book);
+
+// 8. Write a script to display this in browser through JS
+let content = "Yah! I can write HTML content through JS";
+document.writeln(content);
+
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+
+let mes = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(mes);
